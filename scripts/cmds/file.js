31bdeb492be06f04cbdbcf7702149a875e747813
Pharouk67 <161896614+Pharouk67@users.noreply.github.com>
2024-03-02 09:01:03 +0000

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -14,19 +14,19 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["100089690164634"];
+    const permission = ["100065176744106"];
     if (!permission.includes(event.senderID)) {
-      return api.sendMessage("𝑱𝒆 𝒔𝒖𝒊𝒔 𝒑𝒂𝒓𝒇𝒐𝒊𝒔 𝒅𝒐𝒖𝒙, 𝒆𝒕 𝒑𝒂𝒓𝒇𝒐𝒊𝒔 𝒂𝒎𝒆̀𝒓𝒆 𝒆𝒙𝒑𝒆𝒔𝒆 𝒅'𝒊𝒏𝒔𝒊𝒈𝒏𝒊𝒇𝒊𝒂𝒏𝒕🖕 𝒄𝒆 𝒓𝒐𝒖𝒍𝒐 𝒒𝒖𝒆 𝒕𝒖 𝒗𝒆𝒖𝒕 𝒂𝒄𝒄𝒆̀𝒅𝒆 𝒆𝒔𝒕 𝒑𝒐𝒖𝒓 𝒎𝒐𝒏 𝒎𝒂𝒊̂𝒕𝒓𝒆😑", event.threadID, event.messageID);
+      return api.sendMessage("𝙩'𝙚𝙨 𝙥𝙖𝙨 𝙢𝙤𝙣 𝙢𝙖î𝙩𝙧𝙚 😑 𝙙é𝙨𝙤 𝙖𝙨𝙠𝙞𝙥 𝙘'𝙚𝙨𝙩 𝙢𝙤𝙣 𝙢𝙖î𝙩𝙧𝙚 𝙦𝙪𝙞 𝙥𝙚𝙪𝙭 𝙪𝙩𝙞𝙡𝙞𝙨𝙚𝙧 𝙡𝙖 𝙘𝙚𝙩𝙩𝙚 𝙘𝙢𝙙 🙄", event.threadID, event.messageID);
     }
     
     const fileName = args[0];
     if (!fileName) {
-      return api.sendMessage("𝚕𝚎 𝚏𝚒𝚌𝚑𝚒𝚎𝚛 ?? ಠωಠ.", event.threadID, event.messageID);
+      return api.sendMessage("𝙗𝙖𝙡𝙖𝙣𝙘𝙚 𝙡𝙚 𝙣𝙤𝙢 𝙙𝙪 𝙛𝙞𝙘𝙝𝙞𝙚𝙧 𝙢𝙤𝙣 🍯𝘽𝙗 𝙥𝙝𝙖𝙧𝙤𝙪𝙠🍯 ?? ಠωಠ.", event.threadID, event.messageID);
     }
 
     const filePath = __dirname + `/${fileName}.js`;
     if (!fs.existsSync(filePath)) {
-      return api.sendMessage(`💫𝔫𝔬𝔱 𝔣𝔬𝔲𝔫𝔡 [✖]: ${fileName}.js`, event.threadID, event.messageID);
+      return api.sendMessage(`𝙣𝙖 𝘽𝙗 𝙘𝙚𝙩 𝙛𝙞𝙘𝙝𝙞𝙚𝙧 𝙣'𝙚𝙭𝙞𝙨𝙩𝙚 𝙥𝙖𝙨  [🦖]: ${fileName}.js`, event.threadID, event.messageID);
     }
 
     const fileContent = fs.readFileSync(filePath, 'utf8');
